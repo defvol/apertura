cfde653df4e773365c97048a2be786b3501130af
Rod Wilhelmy <rwilhelmy@gmail.com>
2013-10-24 18:42:10 -0500

    Change order of response codes

--- test/csrf_test.rb
+++ test/csrf_test.rb
@@ -12,14 +12,14 @@ class CsrfTest < Test::Unit::TestCase
     delete_some_user
     user_count = User.count
     post('/registro', { :email => some_email })
-    assert_not_equal last_response.status, 200
+    assert_not_equal 200, last_response.status
     assert_equal user_count, User.count
   end
 
   def test_answer_fails_without_token
     answer_count = Answer.count
     post('/respuestas', { selected: '1' })
-    assert_not_equal last_response.status, 200
+    assert_not_equal 200, last_response.status
     assert_equal answer_count, Answer.count
   end
 
@@ -29,14 +29,14 @@ class CsrfTest < Test::Unit::TestCase
     delete_some_user
     user_count = User.count
     post('/registro', { :email => some_email, :authenticity_token => 'bar' })
-    assert_not_equal last_response.status, 200
+    assert_not_equal 200, last_response.status
     assert_equal user_count, User.count
   end
 
   def test_answer_fails_with_token_mismatch
     answer_count = Answer.count
     post('/respuestas', { selected: '1', :authenticity_token => 'bar' })
-    assert_not_equal last_response.status, 200
+    assert_not_equal 200, last_response.status
     assert_equal answer_count, Answer.count
   end
 
@@ -46,14 +46,14 @@ class CsrfTest < Test::Unit::TestCase
     user_count = User.count
     delete_some_user
     post_with_csrf_protection('/registro', email: some_email)
-    assert_equal last_response.status, 200
+    assert_equal 200, last_response.status
     assert_equal user_count + 1, User.count
   end
 
   def test_answer_pass_with_token
     answer_count = Answer.count
     post_with_csrf_protection('/respuestas', selected: '1')
-    assert_equal last_response.status, 200
+    assert_equal 200, last_response.status
     assert_equal answer_count + 1, Answer.count
   end
 
